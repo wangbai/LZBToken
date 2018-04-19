80cf5daf759fcbeed76c262f65cac776ef38ee41
wangbai <wangbai@greencheng.com>
2018-04-19 12:40:23 +0800

    delete deploy

--- script/deploy.js
+++
@@ -1,9 +0,0 @@
-var program = require('commander');
-
-// init web3
-var Web3 = require("web3");
-var web3 = new Web3();
-web3.setProvider(new Web3.providers.HttpProvider("http://localhost:7545"));
-
-var version = web3.version.node;
-console.log(version);
